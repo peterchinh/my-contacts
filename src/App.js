596fd8b48a733273d9d806037db4222a64512e7f
Colin Bruins <colinbruins@gmail.com>
2024-11-07 13:44:41 -0800

    Pre-commit hook setup

--- src/App.js
+++ src/App.js
@@ -25,6 +25,9 @@ export default function App() {
         {sampleContacts.map((contact, index) => (
           <ContactCard
             key={index}
+
+
+
             name={contact.name}
             image={contact.image}
             // onClick= do something later
