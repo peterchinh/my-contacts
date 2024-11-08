6525d2573475d6d3206492f8e329d7227cff81f2
Diego Nieves <denieves@calpoly.edu>
2024-11-08 12:54:50 -0800

    contactInfo appears with backend put & delete

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ContactCard from './component/contact-card';
 import "./style/App.css";
 import NavBar from "./component/navbar";
+import ContactInfo from './component/contact-info';
 
 const sampleContacts = [
     {
       name: "John Smith",
       image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png"
     },
-
     {
       name: "Lebron James",
       image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png"
     }
-  ];
+];
 
 export default function App() {
+    const [selectedContact, setSelectedContact] = useState(null);
+
+    const cardClick = (contact) => {
+        setSelectedContact(contact);
+    };
+
     return (
       <div className="container">
         <NavBar />
         <div className="content">
-          {sampleContacts.map((contact, index) => (
-              <ContactCard
-                key={index}
-                name={contact.name}
-                image={contact.image}
-                // onClick= do something later
+          <div className="contact-list">
+            {sampleContacts.map((contact, index) => (
+              <div key={index} className="contactCard" onClick={() => cardClick(contact)}>
+                <ContactCard
+                  name={contact.name}
+                  image={contact.image}
                 />
+              </div>
             ))}
+          </div>
+          {selectedContact && (
+            <div className="contact-info">
+              <ContactInfo/>
+            </div>
+          )}
         </div>
       </div>
-      );
-    } 
+    );
+}
