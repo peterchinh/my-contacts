54960a5c85a0b31b89d05d5d199871cf62bdb327
Peter Chinh <113927794+peterchinh@users.noreply.github.com>
2024-11-13 19:04:45 -0800

    Fix: App.js merge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import "./style/App.css";
+import Contacts from "./pages/contacts"
 import Login from  "./pages/login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from './pages/signup';
 import "./style/contacts.css"
+
+const sampleContacts = [
+    {
+      name: "John Smith",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png"
+    },
+    {
+      name: "Lebron James",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png"
+    }
+];
 
 export default function App() {
     return (
@@ -11,6 +23,7 @@ export default function App() {
         <div className="content">
           <BrowserRouter>
             <Routes>
+              <Route path='/contacts' element={<Contacts sampleContacts={sampleContacts} />} />
               <Route path='/login' element={<Login />} />
               <Route path='/signup' element={<Signup />} />
             </Routes>
