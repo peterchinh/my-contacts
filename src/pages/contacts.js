7ab2824ca03b738f65dc496a568914b066890aa5
wctlast <113936667+wctlast@users.noreply.github.com>
2024-11-13 18:38:52 -0800

    Add contact
    * Add contact functionality
    
    * added Cancel button to form
    
    * refactored form component into its own file
    
    * Add Contact button on Contact page & phone # formatting

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,23 +1,55 @@
 import React, { useState } from 'react';
 import ContactCard from '../component/contact-card';
 import ContactInfo from '../component/contact-info';
+import ContactForm from '../component/contact-form';
 import NavBar from '../component/navbar';
 import SearchBar from '../component/SearchBar';
 import '../style/contacts.css'
+import axios from 'axios';
 
+// Default contact to send to ContactForm
+const defaultContact = {
+    firstName: '',
+    lastName: '',
+    phone: '',
+    email: '',
+}
 
 function Contacts({ sampleContacts }) {
 
     const [selectedContact, setSelectedContact] = useState(null);
+    const [showContactForm, setShowContactForm] = useState(null);
 
     const cardClick = (contact) => {
         setSelectedContact(contact);
     };
 
+    const toggleContactForm = () => {
+        setShowContactForm(!showContactForm);
+    }
+
+    async function AddContact(contact, didSubmit){
+        if(!didSubmit){
+            toggleContactForm();
+            return;
+        }
+        try {
+            const response = await axios.post("http://localhost:8000/contact", contact);
+            toggleContactForm();
+            return response;
+        } catch (error) {
+            console.log(error);
+            // Not handling errors at the moment
+            toggleContactForm();
+            return false;
+        }
+    }
+
     return (
         <div className="contactpage">
             <NavBar />
             <div className="main">
+                <button className="addcontact" onClick={toggleContactForm}>Add Contact</button>
                 <SearchBar />
                 <div className="contactcontainer">
                     <div className="contactlist">
@@ -35,6 +67,11 @@ function Contacts({ sampleContacts }) {
                     )}
                 </div>
             </div>
+        {showContactForm && (
+            <div className="modal">
+                <ContactForm handleSubmit={AddContact} contact={defaultContact} />
+            </div>
+         )}
         </div>
     );
 };
