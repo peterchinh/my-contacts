8ef73b6508d023d7f0ff7340b0eaaf978c6fe539
Colin Bruins <colinbruins@gmail.com>
2024-12-09 12:10:00 -0800

    setup testing env

---
+++ backend/App.test.js
@@ -0,0 +1,12 @@
+import Contact from "./contact.js";
+import Group from "./group.js";
+import User from "./user.js";
+import axios from "axios";
+
+
+test('adds 1 + 2 to equal 3', () => {
+  // const person = Contact({firstName: "Colin", lastName: "Bruins", 
+  // email: "colin@gmail.com"})
+
+  expect(3).toBe(3);
+})
