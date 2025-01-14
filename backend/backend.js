50fa5ef4677acdc2562b5d8c4e33866d1fdc9412
Colin Bruins <colinbruins@gmail.com>
2025-01-14 14:53:34 -0800

    Added function to add groups

--- backend/backend.js
+++ backend/backend.js
@@ -1,19 +1,19 @@
 // backend.js
-import express from "express";
-import cors from "cors";
-import connectDB from "./database.js";
-import User from "./user.js";
-import Contact from "./contact.js";
-import Group from "./group.js";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import cors from 'cors';
+import connectDB from './database.js';
+import User from './user.js';
+import Contact from './contact.js';
+import Group from './group.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: 'http://localhost:3000',
     credentials: true,
   }),
 );
@@ -21,7 +21,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 
-app.post("/users", async (req, res) => {
+app.post('/users', async (req, res) => {
   try {
     const { name, email, password: plainTextPassword } = req.body;
     const salt = 10;
@@ -31,16 +31,16 @@ app.post("/users", async (req, res) => {
       email,
       password,
     });
-    res.status(201).json({ message: "User created successfully" });
+    res.status(201).json({ message: 'User created successfully' });
   } catch (err) {
     if (err.code === 11000 && err.keyPattern.email) {
-      return res.status(400).json({ error: "Email is already taken" });
+      return res.status(400).json({ error: 'Email is already taken' });
     }
     res.status(400).json({ error: err.message });
   }
 });
 
-app.get("/users", async (req, res) => {
+app.get('/users', async (req, res) => {
   try {
     const users = await User.find();
     res.json(users);
@@ -49,26 +49,26 @@ app.get("/users", async (req, res) => {
   }
 });
 
-app.post("/users/login", async (req, res) => {
+app.post('/users/login', async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email }).lean();
     if (!user) {
-      res.status(404).json({ error: "Email does not have an account" });
+      res.status(404).json({ error: 'Email does not have an account' });
     } else {
       if (await bcrypt.compare(password, user.password)) {
         const accessToken = generateAccessToken({ id: user._id });
-        const refreshToken = generateRefreshToken({id: user._id });
+        const refreshToken = generateRefreshToken({ id: user._id });
 
-        res.cookie("refreshToken", refreshToken, {
+        res.cookie('refreshToken', refreshToken, {
           httpOnly: true,
           secure: false, //Make true in production
-          sameSite: "strict",
+          sameSite: 'strict',
           maxAge: 7 * 24 * 60 * 60 * 1000,
         });
         res.status(200).json({ accessToken });
       } else {
-        res.status(400).json({ error: "Incorrect Password" });
+        res.status(400).json({ error: 'Incorrect Password' });
       }
     }
   } catch (err) {
@@ -76,67 +76,74 @@ app.post("/users/login", async (req, res) => {
   }
 });
 
-app.post("/logout", (req, res) => {
-  res.clearCookie("refreshToken");
-  res.status(200).json({ message: "Logged out" });
+app.post('/logout', (req, res) => {
+  res.clearCookie('refreshToken');
+  res.status(200).json({ message: 'Logged out' });
 });
 
 function generateAccessToken(user) {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "5m" }); // Short-lived JWT
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5m' }); // Short-lived JWT
 }
 
 function generateRefreshToken(user) {
-  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "7d" }); // Long-lived refresh token
+  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' }); // Long-lived refresh token
 }
 
-app.post("/refresh", (req, res) => {
+app.post('/refresh', (req, res) => {
   const refreshToken = req.cookies.refreshToken;
   if (!refreshToken) {
-    return res.status(401).json({ message: "Unauthorized" });
+    return res.status(401).json({ message: 'Unauthorized' });
   }
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
     if (err) {
-      return res.status(403).json({ message: "Forbidden" });
+      return res.status(403).json({ message: 'Forbidden' });
     }
     const newAccessToken = generateAccessToken({ id: user.id });
     return res.status(200).json({ accessToken: newAccessToken });
   });
 });
 
-app.post("/contact", async (req, res) => {
+app.post('/contact', async (req, res) => {
   try {
     const refreshToken = req.cookies.refreshToken;
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, async (err, user) => {
-      if (err) {
-        return res.status(403).json({ message: "Forbidden" });
-      }
-      req.body.user = user.id;
-      const newContact = new Contact(req.body);
-      await newContact.save();
-      res.status(200).json(newContact);
-    });
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+      async (err, user) => {
+        if (err) {
+          return res.status(403).json({ message: 'Forbidden' });
+        }
+        req.body.user = user.id;
+        const newContact = new Contact(req.body);
+        await newContact.save();
+        res.status(200).json(newContact);
+      },
+    );
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 });
 
-app.get("/contact", async (req, res) => {
+app.get('/contact', async (req, res) => {
   try {
     const refreshToken = req.cookies.refreshToken;
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, async (err, user) => {
-      if (err) {
-        return res.status(403).json({ message: "Forbidden" });
-      }
-      const contacts = await Contact.find({ user: user.id });
-      res.json(contacts);
-    });
-    
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+      async (err, user) => {
+        if (err) {
+          return res.status(403).json({ message: 'Forbidden' });
+        }
+        const contacts = await Contact.find({ user: user.id });
+        res.json(contacts);
+      },
+    );
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 });
 
-app.put("/contact/:id", async (req, res) => {
+app.put('/contact/:id', async (req, res) => {
   try {
     const updatedContact = await Contact.findByIdAndUpdate(
       req.params.id,
@@ -144,7 +151,7 @@ app.put("/contact/:id", async (req, res) => {
       { new: true },
     );
     if (!updatedContact) {
-      return res.status(404).json({ error: "Contact not found" });
+      return res.status(404).json({ error: 'Contact not found' });
     }
     res.status(200).json(updatedContact);
   } catch (err) {
@@ -152,65 +159,68 @@ app.put("/contact/:id", async (req, res) => {
   }
 });
 
-app.delete("/contact/:id", async (req, res) => {
+app.delete('/contact/:id', async (req, res) => {
   try {
     const deletedContact = await Contact.findByIdAndDelete(req.params.id);
     if (!deletedContact) {
-      return res.status(404).json({ error: "Contact not found" });
+      return res.status(404).json({ error: 'Contact not found' });
     }
-    res.status(200).json({ message: "Contact deleted successfully" });
+    res.status(200).json({ message: 'Contact deleted successfully' });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 });
 
-app.get("/group", async (req, res) => {
-  try {
-    const updatedContact = await Contact.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true },
-    );
-    if (!updatedContact) {
-      return res.status(404).json({ error: "Contact not found" });
-    }
-    res.status(200).json(updatedContact);
-    const { user } = req.body;
-    const groups = await Group.find({user: user});
-    res.json(groups);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
+app.get('/group', async (req, res) => {
+  const refreshToken = req.cookies.refreshToken;
+  jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
+    async (err, user) => {
+      try {
+        if (err) {
+          return res.status(403).json({ message: 'Forbidden' });
+        }
+        req.body.user = user.id;
+        const groups = await Group.find({ user: user.id });
+        res.json(groups);
+      } catch (err) {
+        res.status(400).json({ error: err.message });
+      }
+    },
+  );
 });
 
-app.post("/group", async (req, res) => {
-  try {
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, async (err, user) => {
-      if (err) {
-        return res.status(403).json({ message: "Forbidden" });
-      }  
-    req.body.user = user.id;
-    const newGroup = new Group(req.body); 
-    await newGroup.save();
-    res.status(200).json(newGroup);
+app.post('/group', async (req, res) => {
+  const refreshToken = req.cookies.refreshToken;
+  jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
+    async (err, user) => {
+      try {
+        if (err) {
+          return res.status(403).json({ message: 'Forbidden' });
+        }
+        req.body.user = user.id;
+        const newGroup = new Group(req.body);
+        await newGroup.save();
+        res.status(200).json(newGroup);
+      } catch (err) {
+        res.status(400).json({ error: err.message });
+      }
+    },
+  );
+});
 
-    });
-
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-})
-
-app.put("/group/:id", async (req, res) => {
+app.put('/group/:id', async (req, res) => {
   try {
     const groupId = req.params.id;
     const { groupName, newContact } = req.body;
     const group = await Group.findById(groupId);
 
     if (!group) {
-      return res.status(404).json({error: "Group not found"})
+      return res.status(404).json({ error: 'Group not found' });
     }
-
 
     if (groupName) {
       group.groupName = groupName;
@@ -219,7 +229,7 @@ app.put("/group/:id", async (req, res) => {
       if (!group.contacts.includes(newContact)) {
         group.contacts.push(newContact);
       } else {
-        return res.status(400).json({error: "Contact already in group"});
+        return res.status(400).json({ error: 'Contact already in group' });
       }
     }
     const updatedGroup = await group.save();
@@ -227,7 +237,7 @@ app.put("/group/:id", async (req, res) => {
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
-})
+});
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, async () => {
