325017961754e2a1df4a847c0d79e63e1dcc498c
BananaKatzu <peterchinh1018@gmail.com>
2025-02-16 20:49:57 -0800

    Rename to fetcher

--- src/hooks/fetchContacts.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const fetchContacts = async (url, order, groupId) => {
-    const response = await axios.get(url, {
-      params: {...order, groupId},
-      withCredentials: true,
-    });
-    return response.data;
-};
