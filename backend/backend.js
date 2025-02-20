ad047e0ddf5e315ed3ec168728023909ba89a5b8
Peter Chinh <peterchinh1018@gmail.com>
2025-02-20 01:06:47 -0800

    Change cookies to production settings

--- backend/backend.js
+++ backend/backend.js
@@ -101,8 +101,8 @@ app.post('/users/login', async (req, res) => {
 
                 res.cookie('refreshToken', refreshToken, {
                     httpOnly: true,
-                    secure: false, //Make true in production
-                    sameSite: 'strict',
+                    secure: true, //Make true in production
+                    sameSite: 'none',
                     maxAge: 7 * 24 * 60 * 60 * 1000,
                 })
                 res.status(200).json({ accessToken })
