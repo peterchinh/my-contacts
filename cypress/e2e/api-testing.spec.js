8b911ec8210daf1c7bc9f63180e38f996bc5b420
Peter Chinh <peterchinh1018@gmail.com>
2025-03-12 21:57:39 -0700

    Fix: Cypress tests

--- cypress/e2e/api-testing.spec.js
+++ cypress/e2e/api-testing.spec.js
@@ -5,7 +5,6 @@
 // 	Then the response code should be 200
 // 	And the response body should be a list of contacts
 
-
 // Feature: User Login
 // 	Scenario: Successfully login
 // 	Given I have valid login credentials
@@ -13,41 +12,63 @@
 // 	Then the response code should be 200
 // 	And I should receive an access and refresh token
 
-const BACKEND_URL = "http://localhost:8000";
+const BACKEND_URL = 'http://localhost:8000'
+let refreshToken
 
-describe("User Login", () => {
-    context("Successfully login", () => {
-        it("GIVEN I have valid login credentials", () => { });
-        
-        it("When I send a POST to /users/login with my credentials", () => {
+describe('User Login', () => {
+    context('Successfully login', () => {
+        it('GIVEN I have valid login credentials', () => {})
+
+        it('When I send a POST to /users/login with my credentials', () => {
             cy.request({
                 method: 'POST',
                 url: `${BACKEND_URL}/users/login`,
                 body: {
-                    email: "cypresstesting@aaaa.com",
-                    password: "Cypress123!"
+                    email: 'cypresstesting@aaaa.com',
+                    password: 'Cypress123!',
                 },
             }).then((response) => {
-                
-                assert.equal(response.status, 200, "Then the response code should be 200");
-                assert.isDefined(response.body.accessToken, "And I received an access token");
-                cy.getCookie("refreshToken").should("exist");
-            });
-        });
-    });
-});
+                assert.equal(
+                    response.status,
+                    200,
+                    'Then the response code should be 200'
+                )
+                assert.isDefined(
+                    response.body.accessToken,
+                    'And I received an access token'
+                )
+                cy.getCookie('refreshToken')
+                    .should('exist')
+                    .then((cookie) => {
+                        refreshToken = cookie.value
+                    })
+            })
+        })
+    })
+})
 
+describe('GET contacts from the API', () => {
+    context('Successfully retrieve list of contacts', () => {
+        it('GIVEN I run the backend', () => {})
 
-
-describe("GET contacts from the API", () => {
-    context("Successfully retrieve list of contacts", () => {
-        it("GIVEN I run the backend", () => { });
-
-        it("When I make a GET request to /contacts", () => {
-            cy.request(`${BACKEND_URL}/contact`).then((response) => {
-                assert.equal(response.status, 200, "Then the response code should be 200");
-                assert.isArray(response.body, "And the response body should be a list of contacts")
-            });
-        });
-    });
-});
+        it('When I make a GET request to /contacts', () => {
+            cy.request({
+                method: 'GET',
+                url: `${BACKEND_URL}/contact`,
+                headers: {
+                    Cookie: `refreshToken=${refreshToken}`, // Ensure the refreshToken is included
+                },
+            }).then((response) => {
+                assert.equal(
+                    response.status,
+                    200,
+                    'Then the response code should be 200'
+                )
+                assert.isArray(
+                    response.body,
+                    'And the response body should be a list of contacts'
+                )
+            })
+        })
+    })
+})
